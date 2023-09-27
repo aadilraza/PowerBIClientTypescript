b7ad2a9437109c3e73e11d2c9a2b9a73b161aaed
Adil Raza <adil.raza@hyperdatacomputing.com>
2023-09-27 19:37:32 +0500

    App own data concept implemented.

--- src/components/report/report.tsx
+++ src/components/report/report.tsx
@@ -13,20 +13,20 @@ const ReportComponent: React.FC<ReportProps> = (props) => {
 
     useEffect(() => {
         const reportElement = reportDomRef.current;
-        if(reportElement) reportElement.style.visibility = 'hidden';
+        if (reportElement) reportElement.style.visibility = 'hidden';
 
         const loaderElement = loaderDomRef.current;
-        if(loaderElement) loaderElement.style.display = 'block';
+        if (loaderElement) loaderElement.style.display = 'block';
     }, []);
 
     useEffect(() => {
         embeddedReport?.on("loaded", () => {
             console.info("Report Loaded");
             const reportElement = reportDomRef.current;
-            if(reportElement) reportElement.style.visibility = 'visible';
+            if (reportElement) reportElement.style.visibility = 'visible';
 
             const loaderElement = loaderDomRef.current;
-            if(loaderElement) loaderElement.style.display = 'none';
+            if (loaderElement) loaderElement.style.display = 'none';
         });
         embeddedReport?.on("rendered", () => {
             console.info("Report Rendered");
@@ -66,14 +66,37 @@ const ReportComponent: React.FC<ReportProps> = (props) => {
         }
     }
 
+    let embedConfiguration = {
+        type: 'report',
+        id: 'fadf9640-5c43-4076-8190-9a01e706a032',
+        embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=fadf9640-5c43-4076-8190-9a01e706a032&groupId=31dc7868-3fa6-47b7-8bf9-3d4a11d7c140&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLU5PUlRILUNFTlRSQUwtRy1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19',
+        accessToken: `H4sIAAAAAAAEACWUx86zWAJE3-XbMhI5jfQvCCabYJJhR7hkfDEZWv3u8033tnRWpVP114-bXQPMyp___mTd0BVcCXs9FIM6FuJBmPi-DmNqLZMIv-m6RWT8FASGioUIorLEF7VZsDPFYS4UJBxeB7PlT0ZlGGcX6TK16tNRzzVM2vchgMvbn0pbSamBsZcVsc_wqN8NQVtdKVG8A0V5V9fA-_K57tkPAZEV4APJzzYkSW9ePNde0vGgSQ8q2uVyUaCbx8-oFTihGEAuFn4KjyBU9Hqntnh5Xzh3t51oumtKR6d0cBKOFMSbE3GqOgqN0trqzIv8mfuvT-d1Ye89wKB40hmQh2MoxYfbtdSe96OWdfUN7W4EnubVaYTogXqD3juANne7Qz0CwQ65loTzO5qF3BZY2MNvd8iDJ-WyvLiruWZbWAnTiZKOBIWJvJBUd0ujqN_dU-EAxbB0nKTG421nRjf5Q3du5eIYBCZeE41faE6qI2HRkR_qilirrfekfAW8G30h3PoSBwmKZooMGio4Q62io2eXA7QbW2eNkPfkZsbZsNFD4GbPsXDgKw3pPkajXiSqTSWalEPD8zBB_YSLkoUqZF5qr2eBXCVnuV7kGN0Bry6NlvhYgaiM9nidsbUjSYKg5KM_GpN3QFXpr7fqbUXx-m6l975tPP3YI2eZD6GOAptovZU2HzrncsitqM1ojZupMW9idgnDmvrLJWR6aeWoYLURLHc5h2FvmiYxEnL3EhDWGMl11uf6JZDuAmzyOTvK8tl8H9K5UBufy4vv58m9uIvvUZumN9UlXE1wlWcDCITnV9yIQ9j1Lrrwy7e4VJieYmxbHwTPA3Xfqntx6A2AdZ8IFEsqaqZ53_7Qe-9xQxFyRIpdmzuP4u2XUS_zrBhBhqr0HonJWSh-w4nL_SwvLpELaoNUEYkr6z9_fv7zI83XtEITXL8zkxIRz8fmHPVFEylVBHRXBvZHk8iVeeyooBWeQHPb48V_cEeTIkJNe4wfThYjaf6laRT79a3BKJBd0irGn-aV1KA89YYhtF3_ILX9AiBjHpq7rGcYs5oJLAOXV3ptldlusUolCo--ibPMahs3uCzIYJRYxjOXuxqDQ6HTDVxveTy-mu0RD-9xclAdfapDhyKi9VnNjytLVnMWsSTo1GyvOtmkgne3DCoWzq5kg4J2U14gFwyMldMe9pfQkELj18-eBSlbR3ckwruZ9nLoES-Mi2Cf5zfiJ4_chSzVnF0VZ5lciYqCRNe8dwh3BDwG-WIYd2k7biy9z4-RX00TfJJgeX68f2u-pgbMevTbcoSlC0jDud3phHQsTqF7Xjv-ofy2_mTrNoNfzJ7RGHs1roYS_mwgK1iXl5qxp_Eq-cgA7OQ9GJ9dew1zu-OD5t2muU6GusrLtqp2TOneY2fAR2rPDHrwnRg-3gp6Id8Ybr49SpdCMwUW2xEekXetIMiuJNhYLScNF25W8TKGEKCbByN14ngrHYAOGY_z9aQyfTx22Jbf7Eeq4S-ztRY0_aQGn4ZtoIkCCoy5rFLaT9ZPsFOTWqhOPru-VTUr5ufON0ssZl1X8OvX76lYCM1UvbaUwWgpuYTMQwV5HOaglyiRoA871GWQc1UkpF5LnoPbI2dIRzIumelXtGXsRoMIDNOi09UwGmFH6Iii-4LRcN-wxOYUeGjv7vZwGOz_nf_7fxnxphlaBgAA.eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLU5PUlRILUNFTlRSQUwtRy1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZXhwIjoxNjk1ODI1ODYzLCJhbGxvd0FjY2Vzc092ZXJQdWJsaWNJbnRlcm5ldCI6dHJ1ZX0=`,
+        tokenType: models.TokenType.Embed,
+        settings: {
+            panes: {
+                filters: {
+                    expanded: false,
+                    visible: false
+                }
+            },
+        }
+    };
+
     return (<>
-        {/* {(!showReport) && <Loader />} */}
+
         <div id="container">
-            <div id="overlay" ref={loaderDomRef}>
+        <PowerBIEmbed
+                    cssClassName="embed-report"
+                    embedConfig={embedConfiguration}
+                    getEmbeddedComponent={(embeddedReport) => {
+                        setEmbeddedReport(embeddedReport as Report);
+                    }}
+                />
+            {/* <div id="overlay" ref={loaderDomRef}>
                 <img id="spinner" alt="Alternate Text" src={loaderGif} />
-            </div>
+            </div> */}
             <div ref={reportDomRef}>
-                <PowerBIEmbed
+                {/* <PowerBIEmbed
                     cssClassName="embed-report"
                     embedConfig={{
                         type: 'report',   // Supported types: report, dashboard, tile, visual and qna
@@ -99,7 +122,9 @@ const ReportComponent: React.FC<ReportProps> = (props) => {
                     getEmbeddedComponent={(embeddedReport) => {
                         setEmbeddedReport(embeddedReport as Report);
                     }}
-                />
+                /> */}
+
+               
             </div>
         </div>
 
