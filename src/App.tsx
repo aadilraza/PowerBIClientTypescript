bc8d2454c31a5c81fcfa336eea3e4e2cf275fcfe
Adil Raza <adil.raza@hyperdatacomputing.com>
2023-10-30 18:10:55 +0500

    Report Id changes.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import './App.css';
 import ReportComponent from './components/report/report';
 
 const App = () => {
-  const tblReportId = '96642825-6f98-4d7a-aa02-d710c1e66149';
-  const dashBoardDownloadedReportId = 'c371310a-f123-455c-a930-03e97e7ac119';
-  const areaChartOneReportId = 'fadf9640-5c43-4076-8190-9a01e706a032';
+  const assetCountReportId = 'b519b417-91c2-446e-9659-587418f93532';
 
   return (
     <div className="App">
-      <ReportComponent reportId={tblReportId} />
-      <ReportComponent reportId={dashBoardDownloadedReportId} />
-      <ReportComponent reportId={areaChartOneReportId} />
+      <ReportComponent reportId={assetCountReportId} />
     </div>
   );
 }
