db1800d66e240de81f3db91a53bc6d07bc08cb8a
Adil Raza <adil.raza@hyperdatacomputing.com>
2023-11-30 19:53:45 +0500

    Dashboard render complete.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ReportComponent from './components/report/report';
+import DashboardComponent from './components/dashboard/dashboard';
 import { models } from 'powerbi-client';
 
 const App = () => {
   const dashboardReportId = 'f4038630-867b-4d7b-9147-6882a6a8fe06';
+  const dashboardId = 'b65a7389-f34f-45af-ae98-035f5eecfe95';
   const filter: models.IBasicFilter = {
     $schema: "http://powerbi.com/product/schema#basic",
     filterType: models.FilterType.Basic,
@@ -19,7 +21,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <ReportComponent reportId={dashboardReportId} filter={filter}/>
+      {/* <ReportComponent reportId={dashboardReportId} filter={filter}/> */}
+      <DashboardComponent dashboardId={dashboardId} />
     </div>
   );
 }
---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { PowerBIEmbed } from 'powerbi-client-react';
+import { Dashboard, IDashboardEmbedConfiguration, IReportEmbedConfiguration, Report, models } from 'powerbi-client';
+import loaderGif from '../loader/giphy.gif';
+import './dashboard.css';
+import axios from 'axios';
+import { DashboardProps } from './dashboardProps';
+
+const DashboardComponent: React.FC<DashboardProps> = ({ dashboardId }) => {
+    const [embeddedApiResponse, setEmbeddedApiResponse] = useState<{ embedUrl: string | undefined, accessToken: string | undefined }>({ embedUrl: undefined, accessToken: undefined });
+    const [embeddedDashboard, setEmbeddedDashboord] = useState<Dashboard>();
+    const reportDomRef = useRef<HTMLDivElement>(null);
+    const loaderDomRef = useRef<HTMLDivElement>(null);
+    const locallyDeployedApi = 'http://localhost:86';
+    const apiUrl = `${locallyDeployedApi}/api/Authentication/GetToken/Dashboard/`;
+    useEffect(() => {
+        const dashboardElement = reportDomRef.current;
+        if (dashboardElement) dashboardElement.style.visibility = 'hidden';
+
+        const loaderElement = loaderDomRef.current;
+        if (loaderElement) loaderElement.style.display = 'block';
+
+        getEmbedToken();
+    }, []);
+
+    useEffect(() => {
+        if (embeddedDashboard) {
+            embeddedDashboard.on("loaded", () => {
+                console.info("Report Loaded");
+                const dashboardElement = reportDomRef.current;
+                const loaderElement = loaderDomRef.current;
+                if (dashboardElement) {
+                    dashboardElement.style.visibility = 'visible';
+                }
+                if (loaderElement) {
+                    loaderElement.style.display = 'none';
+                }
+            });
+            embeddedDashboard.on("rendered", () => {
+                console.info("Report Rendered");
+            });
+        }
+        return () => {
+            embeddedDashboard?.off("loaded");
+            embeddedDashboard?.off("rendered");
+        };
+    }, [embeddedDashboard]);
+
+    const getEmbedToken = () => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+        axios.get(`${apiUrl}${dashboardId}`, config)
+            .then(({ data }) => {
+                setEmbeddedApiResponse({
+                    embedUrl: data.EmbedDashboard[0].EmbedUrl,
+                    accessToken: data.EmbedToken.Token
+                });
+            })
+            .catch(error => console.error(error));
+    }
+
+    let embedConfiguration: IDashboardEmbedConfiguration = {
+        type: 'dashboard',
+        id: dashboardId,
+        embedUrl: embeddedApiResponse?.embedUrl,
+        accessToken: embeddedApiResponse?.accessToken,
+        tokenType: models.TokenType.Embed,
+        settings: {
+            visualSettings: {
+                visualHeaders: [{
+                    settings: { visible: false }
+                }]
+            },
+            panes: {
+                filters: {
+                    visible: false,
+                    expanded: false,
+                }
+            },
+            background: models.BackgroundType.Transparent,
+            filterPaneEnabled: false,
+            navContentPaneEnabled: false
+        }
+    };
+
+    return (
+        <div id="container">
+            <div id="overlay" ref={loaderDomRef}>
+                <img id="spinner" alt="Alternate Text" src={loaderGif} />
+            </div>
+            <div ref={reportDomRef}>
+                <PowerBIEmbed
+                    cssClassName="embed-report"
+                    embedConfig={embedConfiguration}
+                    getEmbeddedComponent={(embeddedDashboard) => {
+                        setEmbeddedDashboord(embeddedDashboard as Dashboard);
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default DashboardComponent;
